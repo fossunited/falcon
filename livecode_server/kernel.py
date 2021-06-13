2c04cba33dfbf0555950e1c43273953c98fe60bf
Anand Chitipothu <anandology@gmail.com>
2021-06-13 06:30:15 +0530

    fixed the issue with reading long lines from docker log
    Docker returns a long line as multiple chunks. Wrote a function to
    combine the chunks and return the output line by line to address this
    issue.

--- livecode_server/kernel.py
+++ livecode_server/kernel.py
@@ -43,9 +43,7 @@ class Kernel:
 
             # TODO: read stdout and stderr seperately
             try:
-                logs = container.log(stdout=True, stderr=True, follow=True)
-                #await self.ws.send_json({"msgtype": "debug", "message": "started container\n"})
-                async for line in logs:
+                async for line in self.read_docker_log_lines(container):
                     if line.startswith("--MSG--"):
                         json_message = line[len("--MSG--"):].strip()
                         msg = json.loads(json_message)
@@ -60,6 +58,37 @@ class Kernel:
                 status = await container.wait()
                 yield {"msgtype": "exitstatus", "exitstatus": status['StatusCode']}
                 await container.delete()
+
+    async def read_docker_log_lines(self, container, max_line_length=1000000):
+        """Reads the docker log line by line.
+
+        When a line is longer, the docker api gives the line in chunks.
+        This function combines the chunks into a line and returns one
+        line at a time as an generator.
+
+        Also includes a protection against very long lines. if a line
+        has more than max_line_length (default 1 million), reading
+        is aborted and no further data is read.
+        """
+        logs = container.log(stdout=True, stderr=True, follow=True)
+
+        remaining = ""
+
+        async for line in logs:
+            line = remaining + line
+            remaining = ""
+
+            if line.endswith("\n"):
+                yield line
+            else:
+                # protection against very large images
+                # stop reading further when the line has more than max_line_length
+                if len(line) > max_line_length:
+                    return
+                remaining = line
+
+        if remaining:
+            yield remaining
 
     def save_file(self, root, filename, contents):
         Path(root, filename).write_text(contents)
