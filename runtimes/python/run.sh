deb306656b36539dd2edd7ff7a531a9bd1eacfd2
Anand Chitipothu <anandology@gmail.com>
2022-02-16 23:56:15 +0530

    Refactored python runtime.
    - Added separate scripts for each mode
    - Made it easier to extend this runtime to add more modes

--- runtimes/python/run.sh
+++ runtimes/python/run.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 TIMEOUT=10
-SOURCE_FILE=${FALCON_SOURCE_FILE:-main.py}
+SOURCE_FILE=${FALCON_SOURCE_FILE:-"main.py"}
+MODE=${FALCON_MODE:-"exec"}
+SCRIPT=/opt/modes/$MODE.sh
 
-if [ "$FALCON_MODE" = "test" ]
+if [ -x "$SCRIPT" ]
 then
-    exec timeout $TIMEOUT python /opt/runtests.py "$@"
+    exec timeout $TIMEOUT $SCRIPT "$@"
 else
-    exec timeout $TIMEOUT python $SOURCE_FILE "$@"
+    echo "ERROR: invalid mode - $MODE" 1>&2
+    exit 1
 fi
