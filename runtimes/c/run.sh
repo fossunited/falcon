2825e26c708c9f9cda62dc07678e241c5d842dd4
Aditya Singh <aditya.singh20@vit.edu.in>
2025-10-10 20:03:52 +0530

    fix: simplify C runtime to properly handle mounted files
    - Streamline run.sh by removing unnecessary copy operations
    - Improve Dockerfile structure and user permission handling

--- runtimes/c/run.sh
+++ runtimes/c/run.sh
@@ -2,9 +2,7 @@
 set -e
 
 SOURCE_FILE="main.c"
-TEMP_SOURCE="/tmp/main.c"
 OUTPUT_BINARY="/tmp/a.out"
 
-cp "${SOURCE_FILE}" "${TEMP_SOURCE}"
-gcc "${TEMP_SOURCE}" -o "${OUTPUT_BINARY}"
+gcc "${SOURCE_FILE}" -o "${OUTPUT_BINARY}"
 exec "${OUTPUT_BINARY}"
