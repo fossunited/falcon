2112796af96f42cdebe894cf585747690019bf50
Aditya Singh <aditya.singh20@vit.edu.in>
2025-10-10 14:30:43 +0530

    Fixed Failing Tests

--- runtimes/c/run.sh
+++ runtimes/c/run.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 set -e
+
 SOURCE_FILE="main.c"
+TEMP_SOURCE="/tmp/main.c"
 OUTPUT_BINARY="/tmp/a.out"
 
-compile_and_run() {
-    gcc "${SOURCE_FILE}" -o "${OUTPUT_BINARY}"
-    exec "${OUTPUT_BINARY}"
-}
-
-if [ "$FALCON_MODE" = "test" ]
-then
-    compile_and_run
-else
-    compile_and_run
-fi
+cp "${SOURCE_FILE}" "${TEMP_SOURCE}"
+gcc "${TEMP_SOURCE}" -o "${OUTPUT_BINARY}"
+exec "${OUTPUT_BINARY}"
